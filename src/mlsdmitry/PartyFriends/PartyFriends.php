ece257fcb9531e069161c8e73fa6bb02cb5c9491
MlsDmitry <43723140+MlsDmitry@users.noreply.github.com>
2020-05-22 17:10:11 +0300

    language files added

--- src/mlsdmitry/PartyFriends/PartyFriends.php
+++ src/mlsdmitry/PartyFriends/PartyFriends.php
@@ -24,15 +24,34 @@ class PartyFriends extends PluginBase
     public function onEnable()
     {
         $this->init_default_config();
-        self::$friends_db = new Config($this->getDataFolder() . DIRECTORY_SEPARATOR . 'friends.yml', Config::YAML);
-        self::$associations = new Config($this->getDataFolder() . DIRECTORY_SEPARATOR . 'associations.json', Config::JSON);
+        self::$friends_db = new Config($this->getDataFolder() . 'friends.yml', Config::YAML);
+        self::$associations = new Config($this->getDataFolder() . 'associations.json', Config::JSON);
         new Lang($this);
+        if (sizeof(glob($this->getDataFolder() . 'languages/' . '*.yml')) === 0) {
+            $this->recurse_copy($this->getFile() . 'resources/languages/', $this->getDataFolder() . 'languages/');
+        }
         $this->getServer()->getCommandMap()->registerAll('PF SYSTEM', [
             new FCommands('f', 'Manage your friends', '/f help', ['friends']),
             new PCommands('p', 'Manage your parties', '/p help', ['party'])
         ]);
         $this->getServer()->getPluginManager()->registerEvents(new PManager(), $this);
         $this->getServer()->getPluginManager()->registerEvents(new FManager(), $this);
+    }
+
+    private function recurse_copy($src, $dst)
+    {
+        $dir = opendir($src);
+        @mkdir($dst);
+        while (false !== ($file = readdir($dir))) {
+            if (($file != '.') && ($file != '..')) {
+                if (is_dir($src . '/' . $file)) {
+                    $this->recurse_copy($src . '/' . $file, $dst . '/' . $file);
+                } else {
+                    copy($src . '/' . $file, $dst . '/' . $file);
+                }
+            }
+        }
+        closedir($dir);
     }
 
     private function init_default_config()
